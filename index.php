747150c624b09a3aad5749da903cb8eb997ccc3c
m-shibata <m-shibata@imjp.co.jp>
2015-06-08 15:52:17 +0900

    バナー作成

---
+++ index.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+
+<html lang="ja">
+<head>
+<title>楽天</title>
+<meta charset="UTF-8">
+<meta content="width=device-width, user-scalable=no" name="viewport">
+<title>no title</title>
+</head>
+<body style="margin-left:20px;">
+	<h3>楽天 - ADNW</h3>
+  <ul>
+		<li>
+			<a target="_blank" href="banner-A.html">Aタイプ</a>
+		</li>
+		<li>
+			<a target="_blank" href="banner-B.html">Bタイプ</a>
+		</li>
+		<li>
+			<a target="_blank" href="banner-G.html">Gタイプ</a>
+		</li>
+		<li>
+			<a target="_blank" href="banner-H.html">Hタイプ</a>
+		</li>
+	</ul>
+
+
+</body>
+</html>
